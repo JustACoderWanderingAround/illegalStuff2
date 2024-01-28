4c612f7feeb61f461d5ade28e69be63f6ac871b4
Helval <valenciayonghel@gmail.com>
2024-01-28 17:55:47 +0800

    push

--- Assets/Scripts/EndingAnimation.cs
+++ Assets/Scripts/EndingAnimation.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class EndingAnimation : MonoBehaviour
 {
@@ -28,5 +29,7 @@ public class EndingAnimation : MonoBehaviour
         sr.sprite = sprites[3];
         yield return new WaitForSeconds(2f);
         sr.sprite = sprites[4];
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("MenuScene");
     }
 }
