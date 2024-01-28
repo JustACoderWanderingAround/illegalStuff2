7feb6bd9121a77db2af45523bd5d4258d254fafa
JustACoderWanderingAround <pgbaquiran@gmail.com>
2024-01-28 16:32:34 +0800

    added adjustable volume controls

---
+++ Assets/Scriptables/Audio/VolumeSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class VolumeSO : ScriptableObject
+{
+    public float mainGroupVolume = 0.5f;
+    public float musicGroupVolume = 0.5f;
+    public float sfxGroupVolume = 0.5f;
+}
